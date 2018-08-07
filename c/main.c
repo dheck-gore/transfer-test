a71c655e852f8ab837249d444a52f8566dceafc5
DHeck <DHeck@wlgore.com>
2018-08-07 08:48:28 -0400

    init

---
+++ c/main.c
@@ -0,0 +1,6 @@
+ int main(int argc, char const *argv[])
+{
+    /* code */
+    printf("Hello, World!");
+    return 0;
+}
---
+++ python/main.py
@@ -0,0 +1,8 @@
+def mult(x,y):
+	return x * y
+
+def main():
+	print ('Hello, world!')
+	print(4,3)
+	
+main()
